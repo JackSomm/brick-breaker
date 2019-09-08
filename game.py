0590a4adcf09abd5edb44de040dd2bf658f79ebb
Jack <jack.somm1@gmail.com>
2019-09-07 18:23:05 -0600

    Added Player class and player movement

--- game.py
+++ game.py
@@ -1,14 +1,17 @@
 import sys, pygame
 pygame.init()
 
-RED = (255,0,0)
+RED = 255, 0, 0
 RECT_WIDTH = 23
 RECT_HEIGHT = 15
 BLACK = 0, 0, 0
+WHITE = 255, 255, 255
 SCREEN = pygame.display.set_mode([800, 600])
+SPEED = [1, 1]
 top = 1
 
 class Rect(pygame.sprite.Sprite):
+  """ This class is the boxes that need to be destroyed in order to win """
   def __init__(self, x, y, color):
     super(Rect, self).__init__()
     self.image = pygame.Surface([RECT_WIDTH, RECT_HEIGHT])
@@ -17,16 +20,36 @@ class Rect(pygame.sprite.Sprite):
     self.rect.x = x
     self.rect.y = y
 
-class Ball(pygame.sprite.Sprite):
-  def __init__(self, x, y, color):
+class Player(pygame.sprite.Sprite):
+  """ This class is the bar at the bottom of the screen that the player controls"""
+  def __init__(self):
+    super(Player, self).__init__()
+    self.height = 15
+    self.width = 100
+    self.image = pygame.Surface([self.width, self.height])
+    self.image.fill(WHITE)
+    self.rect = self.image.get_rect()
+    self.screen_width = pygame.display.get_surface().get_width()
+    self.screen_height = pygame.display.get_surface().get_height()
+    self.rect.x = (self.screen_width / 2) - 50
+    self.rect.y = self.screen_height - 30
 
 
+# Sprite group for rectangles
 rects = pygame.sprite.Group()
 
-for row in range(15):
+player = Player()
+
+# Group to render all sprites
+allsprites = pygame.sprite.Group()
+allsprites.add(player)
+
+# Create the rectangles to be rendered
+for row in range(16):
   for column in range(32):
     rect = Rect(column * (RECT_WIDTH + 2) + 1, top, RED)
     rects.add(rect)
+    allsprites.add(rect)
   top += RECT_HEIGHT + 2
 
 
@@ -38,7 +61,14 @@ def main():
       if event.type == pygame.QUIT: sys.exit()
 
     SCREEN.fill(BLACK)
-    rects.draw(SCREEN)
+    allsprites.draw(SCREEN)
+
+    # Player movement
+    if pygame.key.get_pressed()[pygame.K_LEFT] and player.rect.x != 0:
+      player.rect.x -= SPEED[0]
+    if pygame.key.get_pressed()[pygame.K_RIGHT] and player.rect.x != 800 - player.width:
+      player.rect.x += SPEED[0]
+
     pygame.display.flip()
 
 main()
