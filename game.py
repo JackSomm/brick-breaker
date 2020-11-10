bc9dc73bd8bf36a8891867a45ba2aa6da2405991
Jack Sommer <jack.somm1@gmail.com>
2020-11-10 13:47:31 -0700

    Finished intro screen, added pause function

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, sys
 pygame.init()
 
 RED = 255, 0, 0
@@ -7,10 +7,13 @@ RECT_HEIGHT = 15
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 GREEN = 0, 80, 40
+BRIGHT_GREEN = 0, 90, 40
 RED = 180, 0, 0
+BRIGHT_RED = 200, 0, 0 
 SCREEN = pygame.display.set_mode([800, 600])
-SPEED = 1
+SPEED = 6
 top = 25
+clock = pygame.time.Clock()
 
 class Rect(pygame.sprite.Sprite):
   """ This class is the boxes that need to be destroyed in order to win """
@@ -35,7 +38,7 @@ class Ball(pygame.sprite.Sprite):
     self.screen_height = pygame.display.get_surface().get_height()
     self.x = self.screen_width / 2 - 15 / 2
     self.y = self.screen_height - 47
-    self.speed = 0.5
+    self.speed = 6
     self.direction = 0
 
   def bounce(self, diff):
@@ -91,6 +94,30 @@ class Player(pygame.sprite.Sprite):
       self.rect.x += SPEED
 
 
+def text_objects(msg, font):
+  text = font.render(msg, True, WHITE)
+  
+  return text, text.get_rect()
+
+def button(msg, x, y, w, h, ic, ac, action=None):
+  mouse = pygame.mouse.get_pos()
+  click = pygame.mouse.get_pressed()
+
+  if ((x + w > mouse[0] > x) and (y + h > mouse[1] > y)):
+    pygame.draw.rect(SCREEN, ac, (x, y, w, h))
+
+    if click[0] == 1 and action != None:
+      action()
+  else:
+    pygame.draw.rect(SCREEN, ic, (x, y, w, h))
+
+  sm_text = pygame.font.Font(None, 30)
+  textSurf, textRect = text_objects(msg, sm_text)
+  textRect.center = ((x + (w / 2)), (y + (h / 2)))
+
+  SCREEN.blit(textSurf, textRect)
+
+
 def game_intro():
   intro = True
 
@@ -107,21 +134,13 @@ def game_intro():
     text_pos = [(400 - text_width/2), (300 - text_height)]
     SCREEN.blit(text, text_pos)
 
-    start_button = pygame.draw.rect(SCREEN, GREEN, (270, 370, 120, 40))
-    quit_button = pygame.draw.rect(SCREEN, RED, (420, 370, 120, 40))
+    mouse = pygame.mouse.get_pos()
 
-    sm_text = pygame.font.Font(None, 30)
-
-    start_text = sm_text.render("Start", True, WHITE)
-    quit_text = sm_text.render("Quit", True, WHITE)
-    start_text_pos = [304, 382]
-    SCREEN.blit(start_text, start_text_pos)
-
-    quit_text = sm_text.render("Quit", True, WHITE)
-    quit_text_pos = [456, 382]
-    SCREEN.blit(quit_text, quit_text_pos)
+    button("Start", 270, 370, 120, 40, GREEN,  BRIGHT_GREEN, game_loop)
+    button("Quit", 420, 370, 120, 40, RED,  BRIGHT_RED, quit)
 
     pygame.display.update()
+    clock.tick(15)
 
 # Sprite group for rectangles
 rects = pygame.sprite.Group()
@@ -142,8 +161,6 @@ allsprites.add(ball)
 # Font for game messages
 font = pygame.font.Font(None, 36)
 
-score = 0
-
 # Create the rectangles to be rendered
 for row in range(16):
   for column in range(32):
@@ -152,52 +169,66 @@ for row in range(16):
     allsprites.add(rect)
   top += RECT_HEIGHT + 2
 
-game_over = True
+def game_loop():
+  game_over = False
+  score = 0
+
+  RUNNING, PAUSE = True, False
+  state = RUNNING
+
+  pause_text = font.render("Paused", True, WHITE)
+
+  while not game_over:
+
+    SCREEN.fill(BLACK)
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: sys.exit()
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_p: state = PAUSE
+        if event.key == pygame.K_s: state = RUNNING
+
+    # While game isn't over update player and ball position
+    if not game_over and state == RUNNING:
+      ball.update()
+      player.update()
+    elif state == PAUSE:
+      SCREEN.blit(pause_text, (350, 300))
+
+    # If game is over render "Game Over"
+    if game_over:
+      text = font.render("Game Over!", True, WHITE)
+      text_pos = text.get_rect(centerx=800/2)
+      text_pos.top = 300
+      SCREEN.blit(text, text_pos)
+
+    # If ball collides with player change the direction, takes into account left and right sides for diagonal movement
+    if pygame.sprite.spritecollide(player, balls, False):
+      diff = (player.rect.x + player.width / 2) - (ball.rect.x + ball.width / 2)
+      ball.bounce(diff)
+
+    # Add rects that ball collides with to new group
+    dead_rects = pygame.sprite.spritecollide(ball, rects, True)
+
+    # Keep moving ball as long as there are rects
+    if len(dead_rects) > 0:
+      ball.bounce(0)
+    # End game if rects are 0 or if ball falls off bottom of screen
+    if len(rects) == 0 or ball.y >= 600:
+      game_over = True
+
+    # Increase speed of ball as more rects get hit for difficulty
+    for rect in dead_rects:
+      ball.speed += 0.001
+      score += 2
+
+    score_text = font.render("Score: {0}".format(score), True, WHITE)
+    score_pos = [1, 1]
+    SCREEN.blit(score_text, score_pos)
+
+    allsprites.draw(SCREEN)
+    pygame.display.flip()
+    clock.tick(60)
 
 game_intro()
-
-while not game_over:
-
-  SCREEN.fill(BLACK)
-
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT: sys.exit()
-
-  # While game isn't over update player and ball position
-  if not game_over:
-    ball.update()
-    player.update()
-
-  # If game is over render "Game Over"
-  if game_over:
-    text = font.render("Game Over!", True, WHITE)
-    text_pos = text.get_rect(centerx=800/2)
-    text_pos.top = 300
-    SCREEN.blit(text, text_pos)
-
-  # If ball collides with player change the direction, takes into account left and right sides for diagonal movement
-  if pygame.sprite.spritecollide(player, balls, False):
-    diff = (player.rect.x + player.width / 2) - (ball.rect.x + ball.width / 2)
-    ball.bounce(diff)
-
-  # Add rects that ball collides with to new group
-  dead_rects = pygame.sprite.spritecollide(ball, rects, True)
-
-  # Keep moving ball as long as there are rects
-  if len(dead_rects) > 0:
-    ball.bounce(0)
-  # End game if rects are 0 or if ball falls off bottom of screen
-  if len(rects) == 0 or ball.y >= 600:
-    game_over = True
-
-  # Increase speed of ball as more rects get hit for difficulty
-  for rect in dead_rects:
-    ball.speed += 0.001
-    score += 2
-
-  score_text = font.render("Score: {0}".format(score), True, WHITE)
-  score_pos = [1, 1]
-  SCREEN.blit(score_text, score_pos)
-
-  allsprites.draw(SCREEN)
-  pygame.display.flip()
+game_loop()
